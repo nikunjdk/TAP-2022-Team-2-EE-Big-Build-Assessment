1f131592a8a00f5cb1b6560005bb7afd620dcbfe
imadarshj <geniusadarsh3@gmail.com>
2022-11-03 10:16:15 +0000

    [SA] user bulk upload done

--- prakhar/Storage_Dashboard/Storage_Dashboard/apps/home/routes.py
+++ prakhar/Storage_Dashboard/Storage_Dashboard/apps/home/routes.py
@@ -8,10 +8,15 @@ from flask import Flask
 from apps import new_db
 
 from apps.home import blueprint
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, session
 from flask_login import login_required
 from jinja2 import TemplateNotFound
-
+import pandas as pd
+import os
+from werkzeug.utils import secure_filename
+import mysql.connector as mysql
+from mysql.connector import Error
+ 
 
 class Results(Table):
     user_id = Col('Id', show=False)
@@ -111,7 +116,7 @@ def add_user():
         # validate the received values
         if _name and _login and _onboarded and _department and request.method == 'POST':
             # save edits
-            print("################before executing sql")
+            # print("################before executing sql")
 
             sql = "INSERT INTO users (user_name, user_login, user_onboarded, user_dept) VALUES(%s, %s, %s, %s)"
             data = (_name, _login, _onboarded, _department)
@@ -122,7 +127,7 @@ def add_user():
 
             cursor.execute("SELECT * FROM users")
             rows = cursor.fetchall()
-            print(' ################ User added successfully!')
+            # print(' ################ User added successfully!')
             return redirect(url_for('home_blueprint.users'))
         else:
             return 'Error while adding user'
@@ -130,7 +135,7 @@ def add_user():
         print(e)
     finally:
         cursor.close()
-    conn.close()
+        conn.close()
 
 
 @blueprint.route('/delete/<int:id>')
@@ -150,3 +155,65 @@ def delete_user(id):
     finally:
         cursor.close()
         conn.close()
+
+@blueprint.route('/bulk_add', methods=['POST'])
+@login_required
+def bulk_add_users():
+    print(" ################ in bulk add user form function")
+    
+    UPLOAD_FOLDER = os.path.join('/root/project/TAP-2022-Team-2-EE-Big-Build-Assessment/prakhar/Storage_Dashboard/Storage_Dashboard/apps/static', 'uploads')
+    print(UPLOAD_FOLDER)
+    ALLOWED_EXTENSIONS = {'csv'}
+    try:
+        if request.method == 'POST':
+            uploaded_df = request.files['uploaded-file']
+            # Extracting uploaded data file name
+            print(uploaded_df)
+            data_filename = secure_filename(uploaded_df.filename)
+    
+            # flask upload file to database (defined uploaded folder in static path)
+            uploaded_df.save(os.path.join(UPLOAD_FOLDER, data_filename))
+
+            loadData(os.path.join(UPLOAD_FOLDER, data_filename))
+            return redirect(url_for('home_blueprint.users'))
+        else:
+            print("Error in uploading Data")
+
+    except Exception as e:
+        print(e)
+    finally:
+        print("END")
+        
+
+def loadData(data_file_path):
+    conn = None
+    cursor = None 
+    # read csv file in python flask (reading uploaded csv file from uploaded server location)
+    uploaded_df = pd.read_csv(data_file_path)
+
+    # change fromat of date
+    uploaded_df['onboarded'] = uploaded_df['onboarded'].apply(lambda x : formatDate(x))
+
+    try:
+        conn = mysql.connect(host='localhost', database='USERMGT', user='root', password='Root@123')
+        if conn.is_connected():
+            cursor = conn.cursor()
+            cursor.execute("select database();")
+            record = cursor.fetchone()
+            print("You're connected to database: ", record)
+
+            for i,row in uploaded_df.iterrows():
+                sql = "INSERT INTO USERMGT.users (user_name, user_login, user_onboarded, user_dept) VALUES (%s,%s,%s,%s);"
+                cursor.execute(sql, tuple(row))
+                print("Record inserted")
+                conn.commit()
+    except Error as e:
+                print("Error while connecting to MySQL", e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def formatDate(date_ddmmyyyy):
+    date_dd, date_mm, date_yyyy = date_ddmmyyyy.split("/")
+    return date_yyyy+date_mm+date_dd
